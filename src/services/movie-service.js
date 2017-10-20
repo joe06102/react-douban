a7005d629331a47f3618cd5539afac4ebb68a144
NEXTLABS\jxu <Joe.Xu@nextlabs.com>
2017-10-20 12:59:05 +0800

    fix Url.resolve: make resolve more robust
    Url.resolve now can accept more resources containing '/,string, number, -, _,, .' and splice every resource with /

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -9,29 +9,30 @@ export class MovieService extends BaseService {
         super();
         this.resource = 'movie';
         this.path = Url.resolve(this.version, this.resource);
+        this.movieFetchFactory = new FetchFactory(this.path);
     }
 
     fetchMovieById(id){
-        return FetchFactory.fetchByIdFactory('subject', 'fetchMovieById')(id);
+        return this.movieFetchFactory.fetchByIdFactory('subject', 'fetchMovieById')(id);
     }
 
     fetchCelebrityById(id){
-        return FetchFactory.fetchByIdFactory('celebrity', 'fetchCelebrityById')(id);
+        return this.movieFetchFactory.fetchByIdFactory('celebrity', 'fetchCelebrityById')(id);
     }
     
     fetchMoviesInTheater(queryStringDict){
-        return FetchFactory.fetchByResourceFactory('in_theaters', 'fetchMoviesInTheater')(queryStringDict);
+        return this.movieFetchFactory.fetchByResourceFactory('in_theaters', 'fetchMoviesInTheater')(queryStringDict);
     }
 
     fetchMoviesComingSoon(queryStringDict){
-        return FetchFactory.fetchByResourceFactory('coming_soon', 'fetchMoviesComingSoon')(queryStringDict);
+        return this.movieFetchFactory.fetchByResourceFactory('coming_soon', 'fetchMoviesComingSoon')(queryStringDict);
     }
 
     fetchMoviesOfUSBoxOffice(queryStringDict){
-        return FetchFactory.fetchByResourceFactory('us_box', 'fetchMoviesOfUSBoxOffice', queryStringDict);
+        return this.movieFetchFactory.fetchByResourceFactory('us_box', 'fetchMoviesOfUSBoxOffice', queryStringDict);
     }
 
     fetchTop250MoviesOfDouban(queryStringDict){
-        return FetchFactory.fetchByResourceFactory('top250', 'fetchTop250MoviesOfDouban', queryStringDict);
+        return this.movieFetchFactory.fetchByResourceFactory('top250', 'fetchTop250MoviesOfDouban', queryStringDict);
     }
 }
