6ccbf32f953736afb9a60ff6f895e2bc968fcca6
joe06102 <joe06102@gmail.com>
2017-10-20 17:36:07 +0800

    add dependency
    1. add font-awesome
    2. add index.js for app

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Navbar } from './components/navbar';
+
+ReactDOM.render(<Navbar bgColor='#396'/>, document.querySelector('#root'));
