1909f62c68741b1cc511218817e8f9f4582da495
joe06102 <joe06102@gmail.com>
2017-10-24 14:25:21 +0800

    complete IndicatorList component
    1. add propTypes for IndicatorList
    2. change cursor to pointer when the indicator is hovered

--- src/components/carousel/indicator-list.js
+++ src/components/carousel/indicator-list.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './indicator-list.css';
 import cls from 'classnames';
+import types from 'prop-types';
 
-export const IndicatorList = ({ count, curIndex, jumpTo }) => {
+export const IndicatorList = ({ className, count, curIndex, jumpTo, style }) => {
 
     curIndex = isNaN(Number(curIndex)) ? 0 : Number(curIndex);
     count = isNaN(Number(count)) ? 0 : Number(count);
@@ -21,11 +22,21 @@ export const IndicatorList = ({ count, curIndex, jumpTo }) => {
         return <li className={clsName} data-index={index} key={index}></li>
     });
 
+    const listClsName = cls(styles['indicator-list'], className);
+
     return (
-        <ul className={styles['indicator-list']} onClick={clickHandler}>
+        <ul className={listClsName} onClick={clickHandler} style={style}>
         {
             indicators
         }
         </ul>
     );
 };
+
+IndicatorList.propTypes = {
+    count: types.oneOfType([types.number, types.string]).isRequired,
+    curIndex: types.oneOfType([types.number, types.string]).isRequired,
+    jumpTo: types.func.isRequired,
+    className: types.string,    
+    style: types.object
+}
