42e8570b98dfb91865a9756a9752ea166e70a333
joe06102 <joe06102@gmail.com>
2018-01-15 18:13:49 +0800

    add action creators

---
+++ src/actions/action-creators.js
@@ -0,0 +1,68 @@
+import * as types from './action-types';
+
+const addMovie = (movie) => {
+    return {
+        type: types.ADD_MOVIE,
+        payload: movie
+    }
+}
+
+const addMovies = (movies) => {
+    return {
+        type: types.ADD_MOVIES,
+        payload: movies
+    }
+}
+
+const addCelebrity = (celebrity) => {
+    return {
+        type: types.ADD_CELEBRITY,
+        payload: celebrity
+    }
+}
+
+const addCities = (cities) => {
+    return {
+        type: types.ADD_CITIES,
+        payload: cities
+    }
+}
+
+const setIsFetching = (isFetching) => {
+    return {
+        type: types.SET_IS_FETCHING,
+        payload: isFetching
+    }
+}
+
+const setMovieFetchStatus = (fetchStatus) => {
+    return {
+        type: types.SET_MOVIE_FETCH_STATUS,
+        payload: fetchStatus
+    }
+}
+
+const setCelebrityFetchStatus = (fetchStatus) => {
+    return {
+        type: types.SET_CELEBRITY_FETCH_STATUS,
+        payload: fetchStatus
+    }
+}
+
+const setCitiesFetchStatus = (fetchStatus) => {
+    return {
+        type: types.SET_CITIES_FETCH_STATUS,
+        payload: fetchStatus
+    }
+}
+
+export {
+    addMovie,
+    addMovies,
+    addCelebrity,
+    addCities,
+    setIsFetching,
+    setMovieFetchStatus,
+    setCelebrityFetchStatus,
+    setCitiesFetchStatus,
+}
