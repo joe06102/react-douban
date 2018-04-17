a4d67d32677716fafd15ae0919c44fec42f7b544
joe06102 <joe06102@gmail.com>
2018-04-17 22:03:24 +0800

    fix bug: change class to className

--- src/components/loading-wrapper/index.js
+++ src/components/loading-wrapper/index.js
@@ -6,12 +6,12 @@ const Loading = (WrappedComponent) => props => {
     const { loading, style } = props
 
     return (
-        <div class={styles.wrapper}>
+        <div className={styles.wrapper}>
             <WrappedComponent { ...props }/>
-            <div class={styles.overlay} style={{
+            <div className={styles.overlay} style={{
                 display: loading ? 'inline-block' : 'none'
             }}>
-                <div class={styles.spinner}></div>            
+                <div className={styles.spinner}></div>            
             </div>
         </div>
     )
